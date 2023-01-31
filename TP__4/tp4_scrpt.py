cb39dc5fcb8e9acb619fc8cb734fc355c2fecaeb
Mohamed <hallal.med.abderrahmane@gmail.com>
2023-01-31 14:31:36 +0100

    Homework update

--- TP__4/tp4_scrpt.py
+++ TP__4/tp4_scrpt.py
@@ -1,20 +1,23 @@
-class Binary_tree():
+"""This method prints trees"""
+class Binary():
+    """This class organizes trees"""
     def __init__(self):
         self.root=None
     def print_tree(self,position,position_alternatif = None):
+        """This method prints trees"""
         retour=""
-        for i in range(0, position):
+        for _ in range(0, position):
             retour+="\t"
         retour+=str(self.root.value)+ "/" + str(self.root.depth)
         if position_alternatif:
             position=position_alternatif
         if self.root.left:
             retour += "\n"
-            tree = Binary_tree()
+            tree = Binary()
             tree.root = self.root.left
             retour+= tree.print_tree(position-1)
         if self.root.right:
-            tree = Binary_tree()
+            tree = Binary()
             tree.root = self.root.right
             if self.root.left: # position_alternatif considére ce cas
                 retour += tree.print_tree(2,position)
@@ -23,28 +26,31 @@ class Binary_tree():
                 retour += tree.print_tree(position+2)
         return retour
     def tree_size(self,node):
-        tree=Binary_tree()
+        """ This method returns the number of nodes """
+        tree=Binary()
         tree.root=node
-        count = 0
+        count_lefts = 0
+        count_rights = 0
         if node.left:
-            tree = Binary_tree()
+            tree = Binary()
             tree.root = node.left
-            count = 1 + tree.tree_size(node.left)
+            count_lefts = 1 + tree.tree_size(node.left)
         if node.right:
-            tree = Binary_tree()
+            tree = Binary()
             tree.root = node.right
-            count=  1 + tree.tree_size(node.right)
+            if node.left:
+                count_rights=tree.tree_size(node.right)
+            else: # if we don't add this bloc, the node that have a left and a right child will be counted twice
+                count_rights= 1 + tree.tree_size(node.right)
         if node.is_leaf():
             return 1
-        return count
-
+        return count_lefts + count_rights
 class Node():
     def __init__(self, value):
         self.value=value
         self.right= None
         self.left= None
         self.depth= 0
-
     def add(self, right = None, left = None):
         self.left = left
         self.right = right
@@ -54,7 +60,6 @@ class Node():
         if self.right:
             right.depth = self.depth + 1
             right.update_children_depth()
-
     def update_children_depth(self):
         if self.left:
             self.left.depth = self.depth + 1
@@ -62,7 +67,6 @@ class Node():
         if self.right:
             self.right.depth = self.depth + 1
             self.right.update_children_depth()
-
     def display_node(self):
         retour = str(self)
         if self.right:
@@ -71,14 +75,11 @@ class Node():
             retour += " " + self.left.display_node()
 
         return retour
-
     def __str__(self):
         return str(self.value) + "/" + str(self.depth)
-
     def is_leaf(self):
         #return self.left == None and self.right == None
         return not(self.left or self.right)
-
     def get_max_depth(self,max_depth=0):
         if self.is_leaf():
             if self.depth > max_depth:
@@ -93,17 +94,14 @@ class Node():
             if self.left:
                 max_depth = self.left.get_max_depth(max_depth)
             return max_depth
-
-
 node1 = Node(0)
 node2 = Node(2)
 node3 = Node(3)
 node4 = Node(4)
 node3.add(node4)
 node1.add(node3, node2)
-tree1=Binary_tree()
+tree1=Binary()
 tree1.root=node1
-
 node5 = Node(5)
 node6 = Node(6)
 node7 = Node(7)
@@ -113,10 +111,7 @@ node8 = Node(8)
 #node7.add(node8)
 node7.add(Node(8))
 #tree1.root=node1
-
 #print(str(node1.get_max_depth(0)))
-
-
 #print(node1.display_node())
 print(tree1.print_tree(1))
 print(tree1.tree_size(node1))
